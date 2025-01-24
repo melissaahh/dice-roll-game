446a148110ca42009d563987e93c3e275fd19649
Melissa Harrison <78873771+melissaahh@users.noreply.github.com>
2025-01-24 15:07:43 -0500

    Fixed bonus values boolean

--- Program.cs
+++ Program.cs
@@ -12,12 +12,12 @@ if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
     if ((roll1 == roll2) && (roll2 == roll3)) 
     {
         Console.WriteLine("You rolled triples! +6 bonus to total");
-        total += 2;
+        total += 6;
     }
     else
     {
         Console.WriteLine("You rolled doubles! +2 bonus to total!");
-        total += 6;
+        total += 2;
     }
     
 }
